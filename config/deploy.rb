3715ec7ef072e13a0763d09f0f65597a6f7d94a2
shirokuma-exp <herb9mond@gmail.com>
2020-03-02 13:50:29 +0900

    edit deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,14 @@
+lock '3.12.0'
+set :application, 'chat-space'
+set :repo_url,  'git@github.com:shirokuma-exp/chat-space.git'
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :rbenv_type, :user
+set :rbenv_ruby, '2.5.1' 
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/TeddyKumarin.pem'] 
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
 set :linked_files, %w{ config/secrets.yml }
 
 after 'deploy:publishing', 'deploy:restart'
